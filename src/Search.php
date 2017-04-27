4c9f857da9cd46dd9e67916792a61965325630bf
“luffyzhao <luffyzhao@vip.126.com>
2017-04-27 11:55:07 +0800

    Signed-off-by: “luffyzhao <luffyzhao@vip.126.com>

--- src/Search.php
+++ src/Search.php
@@ -2,7 +2,6 @@
 namespace luffyzhao\helper;
 
 use think\Config;
-use think\Model;
 
 /**
  * 用法
@@ -25,8 +24,6 @@ class Search
     public function __construct()
     {
         $this->request = request();
-        $config = (array) Config::get('search');
-        $this->config = array_merge($this->config, $config);
     }
     /**
      * [getRules description]
@@ -34,17 +31,14 @@ class Search
      * @DateTime 2017-03-02T17:01:59+0800
      * @return   [type]                   [description]
      */
-    public function check(Model $query)
+    public function check(Query $query)
     {
-
-        $params = $this->params();
-        if (empty($params)) {
+        if (empty($this->params())) {
             return $query;
         }
         if (empty($this->rules)) {
             return $query;
         }
-
         $action = $this->request->action();
         if (!isset($this->rules[$action])) {
             return $query;
@@ -53,79 +47,80 @@ class Search
         $rules = $this->rules[$action];
 
         foreach ($rules as $key => $value) {
-            if (isset($params[$key])) {
-                // 查询方法
-                if (!isset($params[$key][0])) {
-                    continue;
-                }
-                $this->checkItem($query, $value, $params[$key], $key);
-            }
+            $this->checkItem($query, $key, $value);
         }
+
+        View::instance(Config::get('template'), Config::get('view_replace_str'))->assign('search', $this->params());
         return $query;
     }
-
     /**
-     * [checkItem description]
-     * @method   checkItem
-     * @DateTime 2017-03-02T17:45:38+0800
-     * @param    [type]                   $item  [description]
-     * @param    [type]                   $value 值
-     * @param    [type]                   $field 字段
-     * @param    [type]                   $query [description]
-     * @return   [type]                          [description]
-     */
-    protected function checkItem(&$query, $item, $value, $field)
-    {
-        switch ($this->getFun($item)) {
-            case 'whereLike':
-                $query->whereLike($this->getField($item, $field), $value . '%');
-                break;
-            case 'whereTime':
-                $query->whereTime($this->getField($item, $field), $this->getOp($item), $value);
-                break;
-            case 'where':
-                $query->where($this->getField($item, $field), $this->getOp($item), $value);
-            case 'scope':
-                $query->scope($value);
-            default:
-                # code...
-                break;
-        }
-    }
-
-    /**
-     * 获取字段
-     * @method   getField
-     * @DateTime 2017-03-02T18:03:11+0800
-     * @param    [type]                   $item    [description]
-     * @param    [type]                   $default [description]
-     * @return   [type]                            [description]
-     */
-    protected function getField($item, $default)
-    {
-        return isset($item[1]) ? $item[1] : $default;
-    }
-
-    /**
-     * 获取查询方法
-     * @method   getFun
-     * @DateTime 2017-03-02T18:01:06+0800
+     * 获取查询条件
+     * @method   getItem
+     * @DateTime 2017-04-25T10:58:01+0800
+     * @param    [type]                   $key  [description]
      * @param    [type]                   $item [description]
      * @return   [type]                         [description]
      */
-    protected function getFun($item, $default = 'where')
+    protected function checkItem(&$query, $key, $item)
     {
-        return isset($item[0]) ? $item[0] : $default;
+        $data = $this->params();
+        if (!isset($data[$key])) {
+            return false;
+        }
+
+        $value = $this->item('value', $key, $item, $data);
+        $field = $this->item('field', $key, $item, $data);
+        $function = $this->item('function', $key, $item, $data);
+        $op = $this->item('op', $key, $item, $data);
+
+        switch ($function) {
+            case 'whereLike':
+                $value = "{$value}%";
+                call_user_func_array([$query, 'whereLike'], [$field, $value]);
+                break;
+            case 'order':
+                call_user_func_array([$query, 'order'], [$value]);
+                break;
+            default:
+                call_user_func_array([$query, $function], [$field, $op, $value]);
+                break;
+        }
+
     }
     /**
-     * 获取表达式
-     * @method   analysisItem
-     * @DateTime 2017-03-02T17:59:27+0800
-     * @return   [type]                   [description]
+     * [value description]
+     * @method   value
+     * @DateTime 2017-04-25T10:59:44+0800
+     * @param    [type]                   $value [description]
+     * @param    [type]                   $item  [description]
+     * @param    [type]                   $data  [description]
+     * @return   [type]                          [description]
      */
-    protected function getOp($item, $default = '=')
+    protected function item($type, $key, $item, array $data)
     {
-        return isset($item[2]) ? $item[2] : $default;
+        $action = request()->action();
+        $method = 'get' . Loader::parseName($action . '_' . $key . '_' . $type, 1) . 'Attr';
+        $method1 = 'get' . Loader::parseName($key . '_' . $type, 1) . 'Attr';
+        if (method_exists($this, $method)) {
+            $default = call_user_func_array([$this, $method], [$data[$key], $item, $data]);
+        } else if (method_exists($this, $method1)) {
+            $default = call_user_func_array([$this, $method1], [$data[$key], $item, $data]);
+        } else {
+            switch ($type) {
+                case 'value':
+                    $default = $data[$key];
+                    break;
+                case 'function':
+                    $default = isset($item[0]) ? $item[0] : 'where';
+                    break;
+                case 'op':
+                    $default = isset($item[2]) ? $item[2] : '=';
+                    break;
+                case 'field':
+                    $default = isset($item[1]) ? $item[1] : $key;
+            }
+        }
+        return $default;
     }
 
     /**
